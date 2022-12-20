afac665a395bfff5e29b5864d071b54b96bfc7dc
Dranoj29 <noj.baysa.29@gmail.com>
2022-12-20 22:19:29 +0800

    Add Simple Email

--- src/main/java/com/dranoj/api/controller/IEmailController.java
+++ src/main/java/com/dranoj/api/controller/IEmailController.java
@@ -1,5 +1,34 @@
 package com.dranoj.api.controller;
 
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+
+import com.dranoj.api.dto.payload.MailRequest;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Email", description = "Endpoint that will be use for sending email")
 public interface IEmailController {
 
+	@Operation(summary = "Send Simple Email")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200",
+				description = "OK",
+				content = {@Content(examples = {@ExampleObject(value = "{\"message\":\"Email Sent\"}")})}),
+			@ApiResponse(responseCode = "500",
+				description = "Error",
+				content = {@Content(examples = {@ExampleObject(value = "{\"Error\":\"Error message\"}")})})
+	})
+	public ResponseEntity<Map<String, String>> sendSimpleEmail(
+			@RequestBody(content = 
+				@Content(examples = 
+					@ExampleObject(value = "{\"to\":[\"baysajonnard@gmail.com\"], \"cc\":[], \"bcc\":[], \"subject\":\"subject\", \"body\":\"body\"}"))) 
+			MailRequest mail);
 }
